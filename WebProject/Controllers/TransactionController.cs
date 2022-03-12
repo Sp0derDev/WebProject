4be77582cf8715ab26af6c90c09f14b6d843da21
Ahmad Safari <60101938@cna-qatar.edu.qa>
2022-03-12 12:59:27 +0300

    Added a Controller for Helper Methods

---
+++ WebProject/Controllers/TransactionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    [Authorize]
+    public class TransactionController : Controller
+    {
+        private WebProjectDbEntities db = new WebProjectDbEntities();
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            //This controller is only for helper methods so there is no index and no authentication. Simply redirect to home page.
+            return RedirectToAction("Index", "Home");
+        }
+
+       
+
+
+    }
+}
