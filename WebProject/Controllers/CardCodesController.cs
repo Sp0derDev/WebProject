df5da2baff43c34abc4f84d3940a9efb6ea3e1f0
Ahmad Safari <60101938@cna-qatar.edu.qa>
2022-03-12 18:25:59 +0300

    Implemented Status Attribute

--- WebProject/Controllers/CardCodesController.cs
+++ WebProject/Controllers/CardCodesController.cs
@@ -49,7 +49,7 @@ namespace WebProject.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,ProductId,Code")] CardCode cardCode)
+        public ActionResult Create([Bind(Include = "Id,ProductId,Code,Status")] CardCode cardCode)
         {
             if (ModelState.IsValid)
             {
@@ -83,7 +83,7 @@ namespace WebProject.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,ProductId,Code")] CardCode cardCode)
+        public ActionResult Edit([Bind(Include = "Id,ProductId,Code,Status")] CardCode cardCode)
         {
             if (ModelState.IsValid)
             {
